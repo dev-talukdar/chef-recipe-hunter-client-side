1eff237cb35d77f725af447cd8c3d4fd4cbf1d91
dev-talukdar <khejurbd@gmail.com>
2023-05-03 22:06:23 +0600

    left nav added

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import img2 from '../../../assets/2.jpg'
+import img3 from '../../../assets/3.jpg'
+import img4 from '../../../assets/4.jpg'
+import img5 from '../../../assets/5.jpg'
+import img6 from '../../../assets/6.jpg'
 
 const LeftNav = () => {
     return (
-        <div>
-           <h2>Left Navigation coming </h2> 
+        <div className='mt-5'>
+            <div className='mt-5'>
+            <img className='img-fluid' src={img6} alt="" />
+            <h4 className='text-center'>Carrot Calley Soup</h4>
+           </div>
         </div>
     );
 };
